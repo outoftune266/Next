03f49fc57b312a3b54db8a7ae8cc531b353cc958
outoftune266 <zakmonnet@gmail.com>
2024-12-13 15:50:26 -0600

    updated dependencies, fixed error in sql insert request

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -3,10 +3,12 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.coerce.number(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
     status: z.enum(['pending', 'paid']),
     date: z.string(),
 });
@@ -23,9 +25,10 @@ export async function createInvoice(formData: FormData) {
     const date = new Date().toISOString().split('T')[0];
 
     await sql`
-    INSERT INTO invoices (customer_id, amount, satus, date)
+    INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
     revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 
 }
